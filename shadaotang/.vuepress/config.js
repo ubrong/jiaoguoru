e2f60cd6583b10edaa2a9965ac478776c255e933
ubrong <ubrong@qq.com>
2023-04-09 11:04:39 +0800

    初版

---
+++ shadaotang/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // port:"88",
+  title: '清修堂 - 酵果乳', 
+  description: '清修堂 - 酵果乳',
+  base:"/jgr/",
+  markdown: {
+    // lineNumbers: false
+  },
+  themeConfig: {
+    nav: [
+      // {
+      //   text: '获得帮助',
+      //   link: '/about.html'
+      // },
+      {
+        text: '咨询购买',
+        link: '/order.html'
+      },
+    ],
+    sidebar: [
+			'/',
+
+    ]
+
+  }
+}
